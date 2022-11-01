40e30c28fd77ab914fe9ef7e10bf7bf03daf1eaf
Kevin Xu <kevinpxu21@gmail.com>
2022-11-01 13:57:19 -0500

    added addEmployee function

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const mysql = require("mysql2");
 const inquirer = require("inquirer");
+const cTable = require("console.table");
 
 //creates a connection to the mysql database
 const db = mysql.createConnection(
@@ -20,8 +21,6 @@ db.connect((err, result) => {
   init();
 });
 
-// init();
-
 async function init() {
   let { choice } = await initChoice();
   switch (choice) {
@@ -38,14 +37,15 @@ async function init() {
       break;
 
     case "Add a department":
-      break;
-    case "Add a department":
+      addDepartment();
       break;
     case "Add a role":
+      addRole(await getDepartmentList());
       break;
     case "Add an employee":
+      addEmployee(await getRoles(), await getManager());
       break;
-    case "Update an employee":
+    case "Update an employee role":
       break;
     default:
       break;
@@ -55,30 +55,32 @@ async function init() {
 //function that prompts the user to choose what to see or do next.
 async function initChoice() {
   console.log("hello");
-  const res = await inquirer.prompt({
+  let res = await inquirer.prompt({
     type: "list",
     message: "What would you like to do?",
     name: "choice",
     choices: [
       "View all departments",
-      "View all roles",
-      "View all employees",
       "Add a department",
+      "View all roles",
       "Add a role",
+      "View all employees",
       "Add an employee",
       "Update an employee role",
       "Quit",
     ],
   });
-  console.log(res);
   return res;
 }
 
 //function that displays all the departments
 function viewDepartments() {
   db.query("SELECT * FROM departments", function (err, results) {
-    const objKeys = Object.keys(results[0]);
-    console.table(results, objKeys);
+    if (err) {
+      console.error(err);
+      return;
+    }
+    console.table(results);
     init();
   });
 }
@@ -88,8 +90,11 @@ function viewRoles() {
   db.query(
     "SELECT roles.roles_id AS roleID, roles.job_title AS title, roles.salary AS salary, departments.departments_name AS department FROM roles JOIN departments ON departments.departments_id = roles.departments_id;",
     function (err, results) {
-      const objKeys = Object.keys(results[0]);
-      console.table(results, objKeys);
+      if (err) {
+        console.error(err);
+        return;
+      }
+      console.table(results);
       init();
     }
   );
@@ -100,9 +105,141 @@ function viewEmployees() {
   db.query(
     'SELECT employees.employees_id AS EmployeeID, CONCAT(employees.first_name, " ", employees.last_name) AS employeeName, employees.manager_name AS managerName, roles.job_title AS jobTitle, roles.salary AS salary, departments.departments_name AS department FROM employees JOIN roles ON roles.roles_id = employees.roles_id JOIN departments ON departments.departments_id = roles.departments_id;',
     function (err, results) {
+      if (err) {
+        console.error(err);
+        return;
+      }
       const objKeys = Object.keys(results[0]);
-      console.table(results, objKeys);
+      console.table(results);
       init();
     }
   );
+}
+
+//adds a Department to the departments table
+async function addDepartment() {
+  let { departmentName } = await inquirer.prompt({
+    type: "input",
+    message: "What is the name of the department",
+    name: "departmentName",
+  });
+
+  db.query(
+    "INSERT INTO departments (departments_name) VALUES (?)",
+    departmentName,
+    (err, result) => {
+      if (err) {
+        console.error(err);
+        return;
+      }
+      console.log("successfully added department");
+    }
+  );
+  init();
+}
+
+function getDepartmentList() {
+  return new Promise((resolve, reject) => {
+    db.query("SELECT departments_name FROM departments", (err, results) => {
+      if (err) {
+        reject(err);
+      }
+      resolve(results.map((object) => object.departments_name));
+    });
+  });
+}
+
+async function addRole(departmentNames) {
+  let { roleName, salary, whichDepartment } = await inquirer.prompt([
+    {
+      type: "input",
+      message: "What is the name of the role?",
+      name: "roleName",
+    },
+    {
+      type: "input",
+      message: "What is the salary for the role?",
+      name: "salary",
+    },
+    {
+      type: "list",
+      message: "Which department does the role belong to?",
+      name: "whichDepartment",
+      choices: departmentNames,
+    },
+  ]);
+
+  db.query(
+    "INSERT INTO roles SET job_title = ?, departments_id = (SELECT departments_id FROM departments WHERE departments.departments_name = ?), salary = ?",
+    [roleName, whichDepartment, salary],
+    (err, results) => {
+      if (err) {
+        console.error(err);
+        return;
+      }
+      console.log("successfully added role");
+    }
+  );
+  init();
+}
+
+function getRoles() {
+  return new Promise((resolve, reject) => {
+    db.query("SELECT job_title FROM roles", (err, results) => {
+      if (err) {
+        reject(err);
+      }
+      resolve(results.map((object) => object.job_title));
+    });
+  });
+}
+
+function getManager() {
+  return new Promise((resolve, reject) => {
+    db.query("SELECT manager_name FROM employees", (err, results) => {
+      if (err) {
+        reject(err);
+      }
+      resolve(results.map((object) => object.manager_name));
+    });
+  });
+}
+
+async function addEmployee(roleName, managerName) {
+  let { firstName, lastName, whichRole, manager } = await inquirer.prompt([
+    {
+      type: "input",
+      message: "What is the employee's first name?",
+      name: "firstName",
+    },
+    {
+      type: "input",
+      message: "What is the employee's last name?",
+      name: "lastName",
+    },
+    {
+      type: "list",
+      message: "What is the employee's role?",
+      name: "whichRole",
+      choices: roleName,
+    },
+    {
+      type: "list",
+      message: "Who is the employee's manager?",
+      name: "manager",
+      choices: managerName,
+    },
+  ]);
+
+  db.query(
+    "INSERT INTO employees SET first_name = ?, last_name = ?, roles_id = (SELECT roles_id FROM roles WHERE roles.job_title = ?), manager_name = ?",
+    [firstName, lastName, whichRole, manager],
+    (err, result) => {
+      if (err) {
+        console.error(err);
+      }
+      console.log("successfully added employee");
+    }
+  );
+  init();
 }
